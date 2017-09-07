cbb48ba09090f9f601228e4527f0eed5cf8c8880
EGOwork <EGOmaniack77@gmail.com>
2017-09-07 16:35:29 +1000

    начало

---
+++ php/ajax/takeData.php
@@ -0,0 +1,8 @@
+<?php
+// ini_set('display_errors', 0) ;
+ini_set('xdebug.var_display_max_depth', 5);
+ini_set('xdebug.var_display_max_children', 256);
+ini_set('xdebug.var_display_max_data', 1024);
+
+var_dump($_FILES);
+?>
